0a92fe14b8ad2fbb353777a04d9ce8afc4e31d0c
Isabella Martínez <44677917+BelisaDi@users.noreply.github.com>
2020-05-01 18:29:56 -0500

    Add files via upload

---
+++ Proyecto/Regresiones/normality_test.r
@@ -0,0 +1,35 @@
+library(readr)
+sk <- read_delim("Estadística/Proyecto/South Korea/new_cases_160420.csv",";", escape_double = FALSE, trim_ws = TRUE)
+
+#Un vistazo inicial a la gráfica
+plot(sk$`New Cases`, type = "l", col= "blue")
+
+#Por comodidad...
+x <- sk$`Number of days`
+y <- sk$`New Cases`
+
+#Este será nuestro modelo, nótese que tiene 4 parámetros y es no lineal
+f <- function(x, theta)  { 
+  m <- theta[1]; s <- theta[2]; a <- theta[3]; b <- theta[4];
+  a*exp(-0.5*((x-m)/s)^2) + b
+}
+
+#Escogemos valores iniciales
+m.0 <- x[which.max(y)]; s.0 <- (max(x)-min(x))/4; b.0 <- min(y); a.0 <- (max(y)-min(y))
+
+#Minimizamos el error cuadrático medio
+fit <- nls(y ~ f(x,c(m,s,a,b)), data.frame(x,y), start=list(m=m.0, s=s.0, a=a.0, b=b.0))
+
+#Resumen del resultado
+summary(fit)
+
+#Gráfica del modelo
+par(mfrow=c(1,1))
+plot(c(x,0),c(y,f(coef(fit)["m"],coef(fit))), main="Data", type="n",
+     xlab="x", ylab="Brightness")
+curve(f(x, coef(fit)), add=TRUE, col="Red", lwd=2)
+points(x,y, pch=19)
+
+#Cálculo de R2
+y_model = f(x, coef(fit))
+R2 = sum((y_model-mean(y))^2)/sum((y-mean(y))^2); R2
---
+++ Proyecto/Regresiones/regresion.r
@@ -0,0 +1,45 @@
+library(readr)
+usa <- read_delim("Estadística/Proyecto/United States/new_cases_160420.csv",";", escape_double = FALSE, trim_ws = TRUE)
+
+#Un vistazo inicial a la gráfica
+plot(usa$`New Cases`, type = "l", col= "blue")
+
+#Vamos a hacer una regresión exponencial. Con fines meramente científicos,
+#diremos que ln(0) = 0 (un matemático llora en la distancia)
+log_new_cases = log(usa$`New Cases`)
+for(i in 1:88){
+  if(is.infinite(log_new_cases[i])){
+    log_new_cases[i] = 0
+  }
+}
+
+#Resumen del modelo
+mod_exp = lm(log_new_cases~usa$`Number of days`)
+summary(mod_exp)
+
+#Parámetros obtenidos
+beta_0 = -2.810111
+beta_1 = 0.156694
+
+#Graficamos el modelo
+par(mfrow=c(1,2))
+plot(usa$`Number of days`,log_new_cases, xlab = "Dias", ylab = "Log del numero de casos",col="red",pch=20,cex=1.5)
+abline(mod_exp,col="blue",lwd=3)
+
+plot(usa$`Number of days`,usa$`New Cases`,xlab = "Dias", ylab = "Numero de casos",col="red",pch=20,cex=1.5)
+curve(exp(beta_0)*exp(beta_1*x),add=T,col="blue",lwd=3)
+
+#Correlación
+cor(usa$`New Cases`,usa$`Number of days`)
+
+#Establezcamos intervalos de confianza del 95% para los parámetros obtenidos.
+alpha = 0.05
+Sxx = sum((usa$`Number of days`-mean(usa$`Number of days`))^2)
+Syy = sum((usa$`New Cases`-mean(usa$`New Cases`))^2)
+Sxy = sum((usa$`Number of days`-mean(usa$`Number of days`))*(usa$`New Cases`-mean(usa$`New Cases`)))
+c00 = sum(usa$`Number of days`^2)/(88*Sxx)
+c11 = 1/Sxx
+SSE = Syy - beta_1*Sxy
+S = sqrt(SSE/86)
+IC_beta0 = c(beta_0 - qt(alpha/2,df = 86, lower.tail = FALSE)*S*sqrt(c00),beta_0 + qt(alpha/2,df = 86, lower.tail = FALSE)*S*sqrt(c00)); IC_beta0
+IC_beta1 = c(beta_1 - qt(alpha/2,df = 86, lower.tail = FALSE)*S*sqrt(c11),beta_1 + qt(alpha/2,df = 86, lower.tail = FALSE)*S*sqrt(c11)); IC_beta1
