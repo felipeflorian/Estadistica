f1a0af2955152e80167ebfde258f00ceef5590ed
felipeflorian <44712473+felipeflorian@users.noreply.github.com>
2020-05-03 17:54:11 -0500

    Add files via upload

---
+++ Proyecto/Regresiones/regresion_muertos_sk.R
@@ -0,0 +1,67 @@
+library(readr)
+sk_deaths <- read_delim("~/Cuarto Semestre/Estadistica/Proyecto/South Korea/cum_confirmed_vs_deaths_160420.csv",";", escape_double = FALSE, trim_ws = TRUE)
+
+totalDeaths <- sk_deaths$Deaths
+
+deathsPerDay <- c(rep(0,length(totalDeaths)))
+
+for(i in 2:length(totalDeaths)){
+  deathsPerDay[i] = totalDeaths[i] - totalDeaths[i-1]
+}
+
+log_deaths = log(totalDeaths)
+log_deaths_per_day = log(deathsPerDay)
+
+for(j in 1:length(log_deaths)){
+  if(is.infinite(log_deaths[j])){
+    log_deaths[j] = 0
+  }
+}
+
+
+for(k in 1:length(log_deaths_per_day)){
+  if(is.infinite(log_deaths_per_day[k])){
+    log_deaths_per_day[k] = 0
+  }
+}
+
+#Resumen del modelo
+model = lm(log_deaths ~ sk_deaths$`Number of days`)
+summary(model)
+
+#Valores obtenidos
+coef(model)
+beta0 = coef(model)[1]
+beta1 = coef(model)[2]
+
+#Modelo para el número de muertes diarias
+model2 = lm(log_deaths_per_day ~ sk_deaths$`Number of days` )
+
+#Gráficas para los modelos 
+par(mfrow=c(1,3))
+plot(sk_deaths$`Number of days`, log_deaths, xlab = "Dias", ylab = "Log del numero de muertes",col="red",pch=20,cex=1.5)
+abline(model, col ='black',lwd=3)
+
+plot(sk_deaths$`Number of days`,sk_deaths$Deaths,xlab = "Dias", ylab = "Muertes acumuladas",col="red",pch=20,cex=1.5)
+curve(exp(beta_0)*exp(beta_1*x),add=T,col="black",lwd=3)
+
+
+plot(sk_deaths$`Number of days`, log_deaths_per_day, xlab = "Dias", ylab = "Log numero de muertes diarias", col = "red",pch = 20, cex = 1.5)
+abline(model2, col = "black",lwd=3)
+
+#Correlación
+cor(sk_deaths$Deaths,sk_deaths$`Number of days`)
+
+#Intervalo de confianza del 95%
+alpha = 0.05
+Sxy = sum((sk_deaths$`Number of days`-mean(sk_deaths$`Number of days`))*(sk_deaths$Deaths-mean(sk_deaths$Deaths)))
+Sxx = sum((sk_deaths$`Number of days`-mean(sk_deaths$`Number of days`))^2)
+Syy = sum((sk_deaths$Deaths-mean(sk_deaths$Deaths))^2)
+SSE = Syy - beta1*Sxy
+c00 = sum((sk_deaths$`Number of days`)^2)/(length(sk_deaths$Deaths)*Sxx)
+c11 = 1/Sxx
+S = sqrt(SSE/(length(sk_deaths$Deaths)-2))
+IC_beta0 = c(beta0 - qt(alpha/2,df = length(sk_deaths)-2, lower.tail = FALSE)*S*sqrt(c00),beta0 + qt(alpha/2,df = length(sk_deaths$Deaths)-2, lower.tail = FALSE)*S*sqrt(c00)) 
+IC_beta0
+IC_beta1 = c(beta1 - qt(alpha/2,df = length(sk_deaths)-2, lower.tail = FALSE)*S*sqrt(c11),beta1 + qt(alpha/2,df = length(sk_deaths$Deaths)-2, lower.tail = FALSE)*S*sqrt(c11))
+IC_beta1
---
+++ Proyecto/Regresiones/regresion_test_USA.R
@@ -0,0 +1,44 @@
+library(readr)
+usa_test <- read_delim("~/Cuarto Semestre/Estadistica/Proyecto/United States/number_tests_170420.csv",";", escape_double = FALSE, trim_ws = TRUE)
+
+number_of_test <- usa_test$Total
+days <- usa_test$`Number of days`
+
+log_test <- log(number_of_test)
+
+for(i in 1:length(number_of_test)){
+  if(is.infinite(log_test[i])){
+    log_test[i] = 0
+  }
+}
+
+#Resumen del modelo
+model <- lm(log_test ~ days)
+summary(model)
+
+#Valores obtenidos
+coef(model)
+beta0 = coef(model)[1]
+beta1 = coef(model)[2]
+
+#Gráficas para los modelos 
+par(mfrow=c(1,2))
+plot(days, log_test, xlab = "Dias", ylab = "Log del numero de test",col="red",pch=20,cex=1.5)
+abline(model, col ='black',lwd=3)
+
+plot(days, number_of_test,xlab = "Dias", ylab = "Test acumulados",col="red",pch=20,cex=1.5)
+curve(exp(beta0)*exp(beta1*x),add=T,col="black",lwd=3)
+
+#Intervalos de confianza del 95%
+alpha = 0.05
+Sxy = sum((days-mean(days))*(number_of_test-mean(number_of_test)))
+Sxx = sum((days-mean(days))^2)
+Syy = sum((number_of_test-mean(number_of_test))^2)
+SSE = Syy - beta1*Sxy
+c00 = sum((days)^2)/(length(days)*Sxx)
+c11 = 1/Sxx
+S = sqrt(SSE/(length(number_of_test)-2))
+IC_beta0 = c(beta0 - qt(alpha/2,df = length(days)-2, lower.tail = FALSE)*S*sqrt(c00),beta0 + qt(alpha/2,df = length(days)-2, lower.tail = FALSE)*S*sqrt(c00)) 
+IC_beta0
+IC_beta1 = c(beta1 - qt(alpha/2,df = length(days)-2, lower.tail = FALSE)*S*sqrt(c11),beta1 + qt(alpha/2,df = length(days)-2, lower.tail = FALSE)*S*sqrt(c11))
+IC_beta1
