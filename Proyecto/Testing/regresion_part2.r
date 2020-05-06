585a54530a99b18f24e8bf11bd4b22f59320dd1b
Isabella Martínez <44677917+BelisaDi@users.noreply.github.com>
2020-05-06 11:26:56 -0500

    Add files via upload

---
+++ Proyecto/Testing/regresion_part2.r
@@ -0,0 +1,22 @@
+# Ahora vamos a comparar nuestro modelo con datos más actualizados!
+
+library(readr)
+library(ggplot2)
+usa_act <- read_delim("Estadística/Proyecto/Datos actualizados/new_cases_usa_060520.csv", ";", escape_double = FALSE, trim_ws = TRUE)
+
+plot(usa_act$`number of days`,usa_act$cases, type="o", col="blue", pch="o", lty=1)
+
+new_data <- usa_act[89:106,]
+new_y <- new_data$cases
+
+# Nuestro modelo
+f <- function(x){ 
+  exp(-2.810111)*exp(0.156694*x)
+}
+
+x = c(89:106)
+y = f(x)
+
+plot(x, new_y, type="o", col="blue", pch="o", lty=1, ylim=range(y))
+points(x, y, col="red", pch="*")
+lines(x, y, col="red",lty=2)
