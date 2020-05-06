55976960f277845cbaed8ee4f0142af976306f66
felipeflorian <44712473+felipeflorian@users.noreply.github.com>
2020-05-06 09:28:53 -0500

    Add files via upload

---
+++ Proyecto/Pruebas de Hipotesis/Estados Unidos/crowded_places_p.R
@@ -0,0 +1,23 @@
+#Encuesta sobre el cambio de habitos entre el 10 y 11 de marzo
+
+#YouGov; Yahoo
+#1635 encuestados telefonicamente
+
+#Pregunta original: ¿Ha realizado algunos cambios a su rutina por el coronavirus? 
+
+#¿Es posible que la minoria de estadounidenses dejaran de asistir a lugares llenos de gente?
+
+#Se hallará el valor p de la siguiente prueba de hipotesis
+
+p_avoid = 0.37
+p_no_avoid = 0.63
+n = 1635
+
+#H_0: p = 0.5 v_s H_q: p < 0.5
+
+z = (p_avoid-0.5)/sqrt(p_avoid*p_no_avoid/n)
+p = pnorm(z, lower.tail = TRUE)
+p
+
+#El valor p es pequeño por lo cual, podemos afirmar que
+#la minoria de la poblacion estadounidense no asiste a lugares llenos de gente
---
+++ Proyecto/Pruebas de Hipotesis/Estados Unidos/stockpiled_items_p.R
@@ -0,0 +1,23 @@
+#Encuesta sobre el cambio de habitos entre el 10 y 11 de marzo
+
+#YouGov; Yahoo
+#1635 encuestados telefonicamente
+
+#Pregunta original: ¿Ha realizado algunos cambios a su rutina por el coronavirus? 
+
+#¿La minoria de los estadounidenses almacenan productos alimenticios?
+
+#Se hallará el valor p de la siguiente prueba de hipotesis
+
+p_yes = 0.15
+p_no = 0.85
+n = 1635
+
+#H_0: p = 0.5 v_s H_q: p < 0.5
+
+z = (p_yes-0.5)/sqrt(p_yes*p_no/n)
+p = pnorm(z, lower.tail = TRUE)
+p
+
+#El valor p es cero es pequeño por lo cual, podemos afirmar que
+#la minoria de la poblacion estadounidense no almacena productos alimenticios
